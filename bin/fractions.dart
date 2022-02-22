f2d5f7b73f4f0ee3423a5cab1c69bdc637940ea5
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2022-02-22 09:34:47 +0300

    Add simple "More" operator

---
+++ bin/fractions.dart
@@ -0,0 +1,14 @@
+import 'package:fractions/fraction.dart';
+
+void main() {
+  var a = Fraction(9, 6);
+
+  var b = Fraction(7, 6);
+
+  var c = Fraction(7, 222);
+
+  print(5 > 7);
+
+  print("$a > $b ? ${a > b}");
+  print("$b > $a ? ${b > a}");
+}
